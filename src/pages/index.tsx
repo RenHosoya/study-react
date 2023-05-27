bbee7c9c27434d6e93b5fdcc7af9e13b030f33f1
r.hosoya <r.hosoya@core-tech.jp>
2023-05-28 06:23:30 +0900

    ⚡️ マウント時、アンマウント時の背景色

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,21 @@ import { Inter } from 'next/font/google'
 import { Links } from '@/src/components/Links'
 import { Headline } from '@/src/components/Headline'
 import { Header } from '@/src/components/Header';
+import { useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  useEffect(() => {
+    console.log("マウント時");
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      console.log("アンマウント時");
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
